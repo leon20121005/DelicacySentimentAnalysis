e0e59bcca826ab3de87f6cef9620df2736f746a4
leon20121005 <leon20121005@gmail.com>
2017-07-30 17:14:13 +0800

    reformat the tab to space

--- Term.java
+++ Term.java
@@ -1,31 +1,31 @@
 public class Term
 {
     private String _term; //詞
-	private String _tag; //詞性
+    private String _tag; //詞性
 
-	public Term(String term, String tag)
-	{
-		_term = term;
-		_tag = tag;
-	}
-
-	public void SetTerm(String term)
+    public Term(String term, String tag)
     {
-		_term = term;
-	}
+        _term = term;
+        _tag = tag;
+    }
 
-	public String GetTerm()
+    public void SetTerm(String term)
     {
-		return _term;
-	}
+        _term = term;
+    }
 
-	public void SetTag(String tag)
+    public String GetTerm()
     {
-		_tag = tag;
-	}
+        return _term;
+    }
 
-	public String GetTag()
+    public void SetTag(String tag)
     {
-		return _tag;
-	}
+        _tag = tag;
+    }
+
+    public String GetTag()
+    {
+        return _tag;
+    }
 }
